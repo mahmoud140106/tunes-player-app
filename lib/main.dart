6256634a27531cf67772b0d824f1bcf8bef0f803
mahmoud140106 <amer140106@gmail.com>
2024-07-20 22:26:59 +0300

    tunes_player_app

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:tunes_player_app/views/tune_view.dart';
 void main() {
   runApp(const TuneApp());
 }
-
+      
 class TuneApp extends StatelessWidget {
   const TuneApp({super.key});
 
